a850446ffe91697e4c9fd300c3502285a76d3b01
Huy <huy@Huys-iMac.local>
2017-02-22 14:29:10 -0500

    Added todos reducer and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,5 +1,7 @@
+import uuid from 'node-uuid';
+import moment from 'moment';
+
 export const searchTextReducer = (state = '', action) => {
-  action.something = 2;
   switch (action.type) {
     case 'SET_SEARCH_TEXT':
       return action.searchText;
@@ -12,6 +14,36 @@ export const showCompletedReducer = (state = false, action) => {
   switch (action.type) {
     case 'TOGGLE_SHOW_COMPLETED':
       return !state;
+    default:
+      return state;
+  }
+};
+
+export const todosReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: uuid(),
+          text: action.text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined,
+        },
+      ];
+      // add case for TOGGLE_TODO
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          let nextCompleted = !todo.completed;
+          return {
+            ...todo,
+            completed: nextCompleted,
+            completedAt: nextCompleted ? moment.unix() : undefined,
+          };
+        }
+      });
     default:
       return state;
   }
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -7,7 +7,7 @@ describe('Reducers', () => {
     it('should set searchText', () => {
       const action = {
         type: 'SET_SEARCH_TEXT',
-        searchText: 'dog',
+        searchText: 'bacon',
       };
       let res = reducers.searchTextReducer(deepFreeze(''), deepFreeze(action));
 
@@ -25,4 +25,36 @@ describe('Reducers', () => {
       expect(res).toBe(true);
     });
   });
+
+  describe('todosReucer', () => {
+    it('should add a new todo', () => {
+      const action = {
+        type: 'ADD_TODO',
+        text: 'Go work exercise'
+      };
+      let res = reducers.todosReducer(deepFreeze([]), deepFreeze(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle a todo', () => {
+      const todos =[{
+        id: '123',
+        text: 'Something',
+        completed: true,
+        createdAt: 123,
+        completedAt: 125,
+      }];
+      const action = {
+        type: 'TOGGLE_TODO',
+        id: '123',
+      };
+      let res = reducers.todosReducer(deepFreeze(todos), deepFreeze(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+    });
+  });
+
 });
